538cfc8145b3e4b95b908fc7752f69fc0a3c8f9d
leo <leo.zuber@cnag.crg.eu>
2023-02-14 16:19:55 +0100

    working version as a simple script

--- metaloci/tools/prep.py
+++ metaloci/tools/prep.py
@@ -1,1 +1,202 @@
-print(test)
+# pylint: disable=invalid-name, wrong-import-position, wrong-import-order
+
+"""
+This script parses a bed file into the proper pickle file needed for METALoci
+"""
+__author__ = "Iago Maceda Porto and Leo Zuber Ponce"
+
+import os.path
+import pathlib
+import re
+import sys
+from argparse import SUPPRESS, ArgumentParser, RawDescriptionHelpFormatter
+from collections import defaultdict
+from datetime import timedelta
+from time import time
+
+import numpy as np
+import pandas as pd
+from tqdm.auto import tqdm
+
+
+description = """
+This script picks a bed file (USCS format) of signals and transforms it into the format  
+needed for METALoci to work.
+Signal names must be in the format CLASS.ID_IND.ID.\n"
+\tClass.ID can be the name of the signal mark.\n"
+\tInd.ID can be the name of the patient/cell line/experiment.\n"""
+
+parser = ArgumentParser(formatter_class=RawDescriptionHelpFormatter,
+                        description=description,
+                        add_help=False)
+
+input_arg = parser.add_argument_group(title="Input arguments")
+
+input_arg.add_argument('-w', '--work-dir', dest='work_dir', required=True,
+                       metavar='PATH', type=str,
+                       help='Path to working directory.')
+
+input_arg.add_argument('-d', '--data', dest='data', required=True,
+                       metavar='PATH', type=str,
+                       nargs="*", action="extend",
+                       help='Path to file to process. The file must contain titles for the columns,'
+                            ' being the first 3 columns coded as chrom, start, end. The following '
+                            'columns should contain the name of the signal coded as: id1_id2.\n'
+                            'Names of the chromosomes must be the same as the coords_file '
+                            'described below. '
+                            '(More than one file can be specified, space separated)')
+
+input_arg.add_argument('-o', '--name', dest='output', required=True,
+                       metavar='STR', type=str,
+                       help='Output file name.')
+
+input_arg.add_argument('-r', '--resolution', dest='reso', required=True,
+                       metavar='INT', type=int,
+                       help="Resolution of the bins to calculate the signal")
+
+region_arg = parser.add_argument_group(title="Region arguments")
+
+region_arg.add_argument('-c', '--coords', dest='coords', required=True,
+                        metavar='PATH', type=str,
+                        help='Full path to a file that contains the name of the chromosomes in the '
+                             'first column and the ending coordinate of the chromosome in the '
+                              'second column.')
+
+optional_arg = parser.add_argument_group(title="Optional arguments")
+
+optional_arg.add_argument('-h', '--help', action="help",
+                          help="Show this help message and exit.")
+
+optional_arg.add_argument('-u', '--debug', dest='debug',
+                          action='store_true', help=SUPPRESS)
+
+args = parser.parse_args(None if sys.argv[1:] else ['-h'])
+
+if not args.work_dir.endswith('/'):
+
+    args.work_dir += '/'
+
+work_dir = args.work_dir
+data = args.data
+out_name = args.output
+coords = args.coords
+resolution = args.reso * 1000
+debug = args.debug
+
+if debug:
+
+    table = [["work_dir", work_dir],
+             ["data", data],
+             ["out_name", out_name],
+             ["centro_and_telo_file", coords],
+             ["binSize", resolution]
+             ]
+    
+    print(table)
+    sys.exit()
+
+
+boundaries = defaultdict(dict)
+
+# Open centromeres and telomeres coordinates file and assign the corresponding values to variables.
+with open(file=coords, mode='r', encoding="utf-8") as chrom:
+
+    for l in chrom:
+
+        line = l.rstrip().split("\t")
+        boundaries[line[0]]['end'] = int(line[1])  # tl_st: end of the initial telomere
+
+file_info = pd.read_table(data[0])
+
+for i in range(1, len(data)):
+
+    temp = pd.read_table(data[i])
+    file_info = pd.merge(file_info, temp, on=["chrom", "start", "end"], how='inner')
+
+col_names = file_info.columns.tolist()
+
+bad_col_names = []
+
+for i in range(3, len(col_names)):
+
+    if len(re.findall("_", col_names[i])) != 1:
+
+        bad_col_names.append(col_names[i])
+
+if len(bad_col_names) > 0:
+
+    print("Problems with the following signal names:")
+    print(', '.join(str(x) for x in bad_col_names))
+    print("Names for signal must be in the following format: CLASS_NAME.")
+    print("Class refers to data that can be potentially merged in downstream analysis.")
+    
+    sys.exit("Exiting due to improper signal names.")
+
+start_timer = time()
+
+for chrm, chrm_value in boundaries.items():
+
+    start_timer_chrm = time()
+
+    estimated_n_bins = int(chrm_value["end"] / resolution ) + 1
+    pbar = tqdm(total = estimated_n_bins)
+
+    print(f"chromosome {chrm.rsplit('r', 1)[1]} in progress. estimated number of bins is {estimated_n_bins}")
+
+    if chrm not in file_info["chrom"].unique():
+
+        print(f"chromosome {chrm.rsplit('r', 1)[1]} not found in the signal file(s), skipping...")
+        continue
+
+    bin_start = 0
+    bin_end = bin_start + resolution
+
+    info = defaultdict(list)
+
+    while bin_start <= chrm_value['end']:
+
+        pbar.update()
+
+        info["Chr"].append(chrm)
+        info["St"].append(bin_start)
+        info["Sp"].append(bin_end)
+
+        tmp_bin = file_info[(file_info['start'] >= int(bin_start)) &
+                           (file_info['end'] <= int(bin_end)) &
+                           (file_info['chrom'] == chrm)]
+
+        # Go over the columns to get all the signals.
+        for j in range(3, tmp_bin.shape[1]):
+
+            if tmp_bin.shape[0] == 0:
+
+                info[col_names[j]].append(np.nan)
+
+            else:
+
+                info[col_names[j]].append(np.nanmedian(tmp_bin.iloc[:, j].tolist()))
+
+        # If the end of the current bin is the start of the terminal telomere, stop
+        if bin_end == boundaries[f"{chrm}"]['end']:
+
+            break
+
+        # Creating tmp variables for an easier check of overlap with centromeres and telomeres.
+        bin_start = bin_end
+        bin_end = bin_start + resolution
+
+    pbar.close()
+
+    # Create a folder to store the pickle file.
+    out_path = os.path.join(work_dir, "signal", f"chr{chrm.rsplit('r', 1)[1]}")  
+    pathlib.Path(out_path).mkdir(parents=True, exist_ok=True)  
+
+    # Creating a pandas DataFrame based on the dictionary created before and save it as pickle.
+    # df_info = pd.DataFrame(info)  
+    pickle_path = os.path.join(work_dir, "signal", f"chr{chrm.rsplit('r', 1)[1]}", f"{out_name}_chr{chrm.rsplit('r', 1)[1]}")
+    pd.DataFrame(info).to_pickle(f"{pickle_path}_signal.pkl")
+
+    # print(f"chromosome {chrm.rsplit('r', 1)[1]} done in {timedelta(seconds=round(time() - start_timer_chrm))}.")
+
+print(f"execution time: {timedelta(seconds=round(time() - start_timer))}")
+print("all done.")
